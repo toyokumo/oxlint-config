04a8acd4f3988c8bea28800ce980355c0bf010fa
egs33 <shuta.eguchi@toyokumo.co.jp>
2026-04-10 17:08:41 +0900

    Add publish config

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { baseConfig } from './base.js';
+export { default as typescriptConfig } from './typescript.js';
+export { default as reactConfig } from './react.js';
+export { default as nextConfig } from './next.js';
+export { default as typescriptJsxConfig } from './typescript-jsx.js';
