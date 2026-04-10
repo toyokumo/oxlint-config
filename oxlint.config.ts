736a27c413ce146db60265f16693b07dc0b24236
egs33 <shuta.eguchi@toyokumo.co.jp>
2026-04-10 16:55:18 +0900

    Add lint and format command

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -2,4 +2,4 @@ import { defineConfig } from 'oxlint';
 import { baseConfig } from './src/base.ts';
 import typescriptConfig from './src/typescript.ts';
 
-export default defineConfig({ ...baseConfig, overrides: [typescriptConfig] });
+export default defineConfig({ ...baseConfig, ignorePatterns: ['tests'], overrides: [typescriptConfig] });
